e94b15b69101d5fc6e195dc938028151e0432c96
parallel <3359531715@qq.com>
2026-09-19 15:54:52 +0800

    更改标题图标

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -36,9 +36,9 @@ export const siteConfig: SiteConfig = {
 		// 顶栏标题文本
 		text: "parallel",
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.webp
-		icon: "assets/home/home.webp",
+		icon: "assets/home/harechan.webp",
 		// 网站Logo图片路径
-		logo: "assets/home/default-logo.webp",
+		logo: "assets/home/harechan.webp",
 	},
 
 	// 页面自动缩放配置
